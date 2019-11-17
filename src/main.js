d3b9e7bbfe9a522f6355a0682e0299dda605bda1
寇倍思 <504815537@qq.com>
2019-11-17 10:34:07 +0800

    清除v.1.0.0不需要的文件和内容

--- src/main.js
+++ src/main.js
@@ -1,10 +1,3 @@
-// import {add,multiply} from "./math.js"
-//
-// const tmp = "hello world"
-// console.log(tmp)
-// console.log(add(3, 2))
-// console.log(multiply(3, 3))
-
 import Vue from "vue"
 
 const app = {
