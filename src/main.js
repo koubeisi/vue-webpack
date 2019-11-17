92b99ffb1827f7fe8c4a9686229c21d3fa5c591d
寇倍思 <504815537@qq.com>
2019-11-17 09:41:20 +0800

    搭建webpack开发环境

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import {add,multiply} from "./math.js"
+
+const tmp = "hello world"
+console.log(tmp)
+console.log(add(3, 2))
+console.log(multiply(3, 3))
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: "development",
+    devtool: 'inline-source-map',       //追踪错误和警告
+    //webpack-dev-server配置，发服务器(dev server)，在哪里查找文件
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "vue_webpack",
+            filename: "index.html",
+            template: './public/index.html'
+        }),
+        new CleanWebpackPlugin()
+    ]
+};
